606c63e6b9a70624924877c6926cd17bcff26529
Elmar <e1imar@outlook.com>
2023-06-11 13:05:06 +0600

    mock api auth endpoints

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,36 +2,60 @@
 import { rest } from 'msw'
 
 export const handlers = [
-  rest.post('/login', (req, res, ctx) => {
-    // Persist user's authentication in the session
-    sessionStorage.setItem('is-authenticated', 'true')
+  // rest.post('/login', (req, res, ctx) => {
+  //   // Persist user's authentication in the session
+  //   sessionStorage.setItem('is-authenticated', 'true')
 
-    return res(
-      // Respond with a 200 status code
-      ctx.status(200),
-    )
-  }),
+  //   return res(
+  //     // Respond with a 200 status code
+  //     ctx.status(200),
+  //   )
+  // }),
 
-  rest.get('/user', (req, res, ctx) => {
-    // Check if the user is authenticated in this session
-    const isAuthenticated = sessionStorage.getItem('is-authenticated')
+  // rest.get('/user', (req, res, ctx) => {
+  //   // Check if the user is authenticated in this session
+  //   const isAuthenticated = sessionStorage.getItem('is-authenticated')
 
-    if (!isAuthenticated) {
-      // If not authenticated, respond with a 403 error
+  //   if (!isAuthenticated) {
+  //     // If not authenticated, respond with a 403 error
+  //     return res(
+  //       ctx.status(403),
+  //       ctx.json({
+  //         errorMessage: 'Not authorized',
+  //       }),
+  //     )
+  //   }
+
+  //   // If authenticated, return a mocked user details
+  //   return res(
+  //     ctx.status(200),
+  //     ctx.json({
+  //       username: 'admin',
+  //     }),
+  //   )
+  // }),
+
+  
+  rest.post('/login', ({body}, res, ctx) => {
+    const savedPassword = JSON.parse(sessionStorage.getItem(body.login))?.password
+    if (savedPassword === body.password) {
+      sessionStorage.setItem('is-authenticated', 'true')
       return res(
-        ctx.status(403),
-        ctx.json({
-          errorMessage: 'Not authorized',
-        }),
+        ctx.status(200),
       )
     }
 
-    // If authenticated, return a mocked user details
-    return res(
-      ctx.status(200),
-      ctx.json({
-        username: 'admin',
-      }),
-    )
+    return res(ctx.status(403))
+  }),
+
+  rest.post('/signin', ({body}, res, ctx) => {
+    if (sessionStorage.getItem(body.login)) return res(ctx.status(403))
+    if (body.login && body.password && body.number) {
+      const userData = JSON.stringify({password: body.password, number: body.number})
+      sessionStorage.setItem(body.login, userData)
+      return res(ctx.status(200))
+    }
+
+    return res(ctx.status(403))
   }),
 ]
