23b460c5569bfa9b268f972dd05000bd8392e9a4
Elmar <e1imar@outlook.com>
2023-06-13 15:27:38 +0600

    add /reserve in app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
+import './App.css' 
 import { useRoutes } from 'react-router-dom'
 import Reservations from './components/Reservations'
 import Login from './components/Login'
 import SignIn from './components/SignIn'
+import ResForm from './components/ResForm'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App () {
+const App = () => {
   const routes = useRoutes([
     {path: '/', element: <Reservations/>},
     {path: '/login', element: <Login/>},
     {path: '/signin', element: <SignIn/>},
+    {path: '/reserve', element: <ResForm/>},
   ])
 
   return routes
