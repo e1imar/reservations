9cc6d1fcdc608d4e53b8447b034b3ec94f83367d
Elmar <e1imar@outlook.com>
2023-06-13 15:50:58 +0600

    reserve from component

---
+++ src/components/ResForm.jsx
@@ -0,0 +1,60 @@
+import React, {useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
+import Form from 'react-bootstrap/Form'
+import Tables from './Tables'
+import Alert from 'react-bootstrap/Alert';
+
+const ResForm = () => {
+  const navigate = useNavigate(),
+  logedIn = sessionStorage.getItem('is-authenticated'),
+
+  newDate = new Date(),
+  today = newDate.toISOString().split('T')[0],
+  timeNow = newDate.toLocaleTimeString([], { hour: '2-digit'}),
+  inputTime = timeNow >= 12 ? (Number(timeNow) + 1) : 12,
+
+  [date, setDate] = useState(today),
+  [time, setTime] = useState(inputTime),
+  [personQ, setPersonQ] = useState(1),
+
+  startTime = date === today ? inputTime : 12,
+  isPastDate = new Date(date) < new Date (today)
+
+  let timeOptions = [], personOptions = []
+
+  for (let index = startTime; index <= 22; index++) {
+    timeOptions.push(<option key={index} value={index}>{index}:00</option>)
+  }
+  for (let index = 1; index <= 6; index++) {
+    personOptions.push(<option key={index} value={index}>{index}</option>)
+  }
+  
+  useEffect(() => {
+    if (logedIn !== 'true') navigate('/login')
+  }, [logedIn])
+
+  return <>
+    <h1 className="mb-3">Reserve a table</h1>
+    <Form className="mb-3">
+      <Form.Group className="mb-3 me-4 d-inline-block">
+        <Form.Label>Date:</Form.Label>
+        <Form.Control type="date"  min={today} value={date} onChange={e => setDate(e.target.value)}/>
+      </Form.Group>
+      <Form.Group className="mb-3 me-4 d-inline-block">
+        <Form.Label>Time:</Form.Label>
+        <Form.Select min={inputTime} value={time} onChange={e => setTime(e.target.value)} disabled={isPastDate}>
+          {timeOptions}
+        </Form.Select>
+      </Form.Group>
+      <Form.Group className="mb-3 me-4 d-inline-block">
+        <Form.Label>Persons:</Form.Label>
+        <Form.Select onChange={e => setPersonQ(e.target.value)} value={personQ} disabled={isPastDate}>
+          {personOptions}
+        </Form.Select>
+      </Form.Group>
+    </Form>
+    {isPastDate ? <Alert variant="warning">Choose correct date</Alert> : <Tables personQ={personQ} time={time} date={date}/>}
+  </>
+}
+
+export default ResForm
