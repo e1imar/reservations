c31921623eb45daea80b8bae244e4344f8fdf7e0
Elmar <e1imar@outlook.com>
2023-06-14 16:05:30 +0600

    add request reserve function in Table button

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,15 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Modal from 'react-bootstrap/Modal';
+import Alert from 'react-bootstrap/Alert';
+import { useAddResMutation } from '../services/reservation';
 
-const Table = ({name, disabled}) => {
-  return <Card className="d-inline-block">
+const Table = ({name, date, time, personQ, reserves, id: tableID, tableGroup}) => {
+  const [addRes, response] = useAddResMutation(),
+  [show, setShow] = useState(false),
+  disabled = reserves[date]?.includes(Number(time)),
+
+  handleClose = () => setShow(false),
+  handleShow = () => setShow(true),
+
+  handleRes = e => {
+    e.preventDefault()
+    addRes({date, time, personQ, tableID, tableGroup})
+  },
+
+  ConfirmRes = <Modal show={show} onHide={handleClose} centered>
+    <Modal.Header closeButton>
+      <Modal.Title>
+        {response.isSuccess ? <Alert variant="success">Successfull reserve</Alert> : 'Check details'}
+      </Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <ul>
+        <li>{name}</li>
+        <li>For {personQ} {personQ > 1 ? 'persons' : 'person'}</li>
+        <li>Date: {date}</li>
+        <li>Time: {time}</li>
+      </ul>
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>
+        Close
+      </Button>
+      <Button variant="primary" onClick={handleRes}  disabled={disabled}>
+        Reserve
+      </Button>
+    </Modal.Footer>
+  </Modal>
+
+  return <Card className="d-inline-block" border={response.isSuccess ? "success" : ""}>
     <Card.Body>
       <Card.Title>{name}</Card.Title>
       <Card.Text>
+        {disabled ? 'Reserved' : "Available"}
       </Card.Text>
-      <Button variant="primary" disabled={disabled}>Reserve</Button>
+      <Button variant="primary" onClick={handleShow} disabled={disabled}>Reserve</Button>
     </Card.Body>
+    {ConfirmRes}
   </Card>
 }
 
--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -3,17 +3,17 @@ import { useGetTablesQuery } from '../services/reservation'
 import ListGroup from 'react-bootstrap/ListGroup';
 import Table from './Table'
 
-const Tables = ({personQ, time, date}) => {
+const Tables = (props) => {
   const {data, isLoading, error} = useGetTablesQuery()
 
   if (isLoading) return <div>Loading...</div>
   if (error) return <div>{error}</div>
 
   return <ListGroup>
-    {Object.keys(data).filter(persons => persons >= personQ).map(tableGroup => <ListGroup.Item key={tableGroup}>
+    {Object.keys(data).filter(persons => persons >= props.personQ).map(tableGroup => <ListGroup.Item key={tableGroup}>
       <h2 className="mb-3">Tables for {tableGroup} persons</h2>
       {data[tableGroup]
-      .map((table, i) => <Table key={table.id} name={'Table ' + (i+1)} disabled={table.reserves[date]?.includes(Number(time))}/>)}
+      .map((table, i) => <Table key={table.id} name={'Table ' + (i+1)} {...props} {...table} tableGroup={tableGroup}/>)}
     </ListGroup.Item>)}
   </ListGroup>
 }
