98731bef951ea6733983ff00c2db366c51aa307c
Elmar <e1imar@outlook.com>
2023-06-14 19:42:14 +0600

    list of reserves

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import { useGetReservationsQuery } from '../services/reservation'
 import NavBarWrapper from './NavBarWrapper'
+import ListGroup from 'react-bootstrap/ListGroup'
+import MadeRes from './MadeRes'
+import { useGetReservationsQuery } from '../services/reservation'
 
 const Reservations = () => {
   const {data, isLoading, error} = useGetReservationsQuery()
 
   if (isLoading) return <div>Loading...</div>
-  if (error) return <div>{error}</div>
+  if (error) return <><div>{error.status}</div><div>{error.error}</div></>
+  
+  const list = <ListGroup className="d-flex flex-row flex-wrap">
+    {data?.map(res => <MadeRes key={res.tableID} {...res}>Cras justo odio</MadeRes>)}
+  </ListGroup>
 
   return <NavBarWrapper>
-    <h1>Resetvations</h1>
-    {data}
+    <h1 className="mb-3">Reservations</h1>
+    {list}
   </NavBarWrapper>
 }
 
