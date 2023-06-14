28c96c63d0cbceb80a8fe21a0ffc8c3580f0d592
Elmar <e1imar@outlook.com>
2023-06-14 14:55:35 +0600

    add reserve post rtk query

--- src/services/reservation.js
+++ src/services/reservation.js
@@ -3,14 +3,28 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const reservationApi = createApi({
   reducerPath: 'reservationApi',
   baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  tagTypes: ['Reserve'],
   endpoints: builder => ({
     getReservations: builder.query({
       query: () => ``,
+      providesTags: ['Reserve'],
     }),
     getTables: builder.query({
-      query: () => `reserve`,
+      query: () => `Reserve`,
+      providesTags: ['Reserve']
+    }),
+    addRes: builder.mutation({
+      query: (payload) => ({
+        url: 'Reserve',
+        method: 'POST',
+        body: payload,
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      }),
+      invalidatesTags: ['Reserve'],
     }),
   }),
 })
 
-export const { useGetReservationsQuery, useGetTablesQuery } = reservationApi
+export const { useGetReservationsQuery, useGetTablesQuery, useAddResMutation } = reservationApi
