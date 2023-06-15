98f2797d1823158cc0626acb13b54d4909a756d0
Elmar <e1imar@outlook.com>
2023-06-15 15:51:12 +0600

    change error handling

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -1,24 +1,13 @@
-import React, { useEffect} from 'react'
+import React from 'react'
 import NavBarWrapper from './NavBarWrapper'
 import ListGroup from 'react-bootstrap/ListGroup'
 import MadeRes from './MadeRes'
-import { useNavigate } from 'react-router-dom'
 import { useGetReservationsQuery } from '../services/reservation'
 
 const Reservations = () => {
-  const {data, isLoading, error} = useGetReservationsQuery(),
-  navigate = useNavigate()
-  
-  useEffect(() => {
-    if (error.data.errorMessage === 'Not authorized') navigate('/login')
-  }, [error])
+  const {data, isLoading, error} = useGetReservationsQuery()
 
   if (isLoading) return <div>Loading...</div>
-  if (error) return <>
-  <div>{error.status}</div>
-  <h1>{error.error}</h1>
-  <div>No reservations</div>
-  </>
   
   const list = <ListGroup className="d-flex flex-row flex-wrap">
     {data?.map(res => <MadeRes key={res.tableID} {...res}>Cras justo odio</MadeRes>)}
@@ -27,6 +16,10 @@ const Reservations = () => {
   return <NavBarWrapper>
     <h1 className="mb-3">Reservations</h1>
     {list}
+    {error && <>
+      <h2>{error.status}</h2>
+      <div>No reservations</div>
+    </>}
   </NavBarWrapper>
 }
 
