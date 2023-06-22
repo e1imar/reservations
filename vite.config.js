ad91bd52f55ef5a1477e3ea6d3c4d2f31b4f49f1
Elmar <e1imar@outlook.com>
2023-06-22 13:41:40 +0600

    add browsers & esbuildOptions

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {target: 'es2015'}
+  build: {target: ['es2015', 'edge88', 'firefox78', 'chrome87', 'safari14']},
+  optimizeDeps: {
+    esbuildOptions: { target: 'es2015'},
+  }
 })
