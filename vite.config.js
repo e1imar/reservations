ce6d0009709e9abc9123a8c615606b9214b8695f
Elmar <e1imar@outlook.com>
2023-06-22 13:53:08 +0600

    add vitejs/plugin-legacy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+    }),
+    react()
+  ],
   build: {target: ['es2015', 'edge88', 'firefox78', 'chrome87', 'safari14']},
   optimizeDeps: {
     esbuildOptions: { target: 'es2015'},
